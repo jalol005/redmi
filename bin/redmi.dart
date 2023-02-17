5354ff0ea0fcfd297f0774041db4a1ff3cd48e4b
Mirjalol <jalolubaydullayev575@gmail.com>
2023-02-17 14:17:23 +0500

    redmi

---
+++ bin/redmi.dart
@@ -0,0 +1,23 @@
+import 'package:redmi/redmi.dart' as redmi;
+
+void main(){
+  print ('==========\\O /===========');
+  print ('| MOBIUZ   \\/   12:12    |');
+  print ('|                        |');
+  print ('|                        ||');
+  print ('|                        ||');
+  print ('|                        ||');
+  print ('|                        |');
+  print ('|                        |');
+  print ('|                        ||');
+  print ('|                        ||');
+  print ('|                        |');
+  print ('|                        |');
+  print ('|                        |');
+  print ('|                        |');
+  print ('|                        |');
+  print ('|                        |');
+  print ('|      REDMI NOTE 9      |');
+  print ('| []         O         > |');
+  print ('==========================');
+}
